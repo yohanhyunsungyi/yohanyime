024c045677f146bfc7123a5a4d6e145597f6b27a
Yohan Hyunsung Yi <yohanhyunsungyi@gmail.com>
2023-05-03 02:24:40 -0700

    created mainpage

--- Mainpage/views.py
+++ Mainpage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'iOSProjects/post_list.html', {})
+    return render(request, 'Mainpage/post_list.html', {})
