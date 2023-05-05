802e3f353ef74b1e01da4f05cbdaad38df1e3417
Yohan Hyunsung Yi <yohanhyunsungyi@gmail.com>
2023-05-05 14:41:24 -0700

    update

--- static/js/script.js
+++ static/js/script.js
@@ -368,3 +368,20 @@ $(document).ready(function($) {
 function Resize() {
 	$(window).trigger('resize');
 }
+
+/* ---------------------------------------------------------------------- */
+/*	Medium Post
+/* ---------------------------------------------------------------------- */
+
+// import { hello } from './module.js';
+
+// var container = document.getElementById("retainable-rss-embed");
+// if (container) {
+//     var css = document.createElement('link');
+//     css.href = "https://www.twilik.com/assets/retainable/rss-embed/retainable.css";
+//     css.rel = "stylesheet"
+//     document.getElementsByTagName('head')[0].appendChild(css);
+//     var script = document.createElement('script');
+//     script.src = ./mediumPost.js;
+//     document.getElementsByTagName('body')[0].appendChild(script);
+// }
